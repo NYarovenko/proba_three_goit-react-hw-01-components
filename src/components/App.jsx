e7015f355fe22726dbe75ce7c90b3d487202935d
Nikolay <NYarovenko.Full.Stack@gmail.com>
2025-09-22 00:30:29 +0300

    Statistics_start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,28 @@
 import user from 'data/user.json';
+import data from 'data/data.json';
+
 import { Profile } from './Profile';
+import { Statistics } from './statistics/Statistics';
 
 export const App = () => {
   return (
-    <Profile
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
-    />
+    <div
+      style={{
+        marginTop: 16,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+
+      <Statistics title="Upload stats" stats={data} />
+    </div>
   );
 };
